7bbcb97c71910c52ea84c3dc3c19f87c2d9e4c67
Tiziano Santoro <tiziano88@gmail.com>
2017-12-30 14:25:52 +0100

    Use rgb crate for pixels
    rgb::RGB8 is equivalent to the type currently used, though this is a
    breaking API change, hence the API version bump.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ extern crate spidev;
 
 extern crate failure;
 
+extern crate rgb;
+
 #[cfg(feature = "fake-hardware")]
 use ansi_term::Color::RGB;
 #[cfg(feature = "fake-hardware")]
@@ -17,6 +19,8 @@ use std::io::prelude::*;
 
 #[cfg(feature = "hardware")]
 use spidev::{SPI_MODE_0, Spidev, SpidevOptions};
+
+const BLACK: rgb::RGB8 = rgb::RGB8 { r: 0, g: 0, b: 0 };
 
 /// Possible rotations of the buffer before displaying to the
 /// Unicorn HAT HD.
@@ -34,7 +38,7 @@ pub enum Rotate {
 #[cfg(feature = "hardware")]
 /// Provide high-level access to the Unicorn HAT HD.
 pub struct UnicornHatHd {
-    leds: [UnicornHatHdLed; 256],
+    leds: [rgb::RGB8; 256],
     spi: Spidev,
     rotation: Rotate,
 }
@@ -42,7 +46,7 @@ pub struct UnicornHatHd {
 #[cfg(feature = "fake-hardware")]
 /// Provide high-level access to an emulated Unicorn HAT HD.
 pub struct UnicornHatHd {
-    leds: [UnicornHatHdLed; 256],
+    leds: [rgb::RGB8; 256],
     rotation: Rotate,
 }
 
@@ -65,7 +69,7 @@ impl UnicornHatHd {
             .build();
         try!(spidev.configure(&options));
         Ok(UnicornHatHd {
-            leds: [UnicornHatHdLed::default(); 256],
+            leds: [BLACK; 256],
             spi: spidev,
             rotation: Rotate::RotNone,
         })
@@ -77,7 +81,7 @@ impl UnicornHatHd {
     /// `_spi_path` is completely unused by the fake `UnicornHatHd`.
     pub fn new(_spi_path: &str) -> Result<UnicornHatHd, Error> {
         Ok(UnicornHatHd {
-            leds: [UnicornHatHdLed::default(); 256],
+            leds: [BLACK; 256],
             rotation: Rotate::RotNone,
         })
     }
@@ -121,8 +125,8 @@ impl UnicornHatHd {
     ///
     /// The origin (`(0, 0)`) is the top-left of the display, with `x` & `y`
     /// increasing to the right, and down, respectively.
-    pub fn set_pixel(&mut self, x: usize, y: usize, r: u8, g: u8, b: u8) {
-        self.leds[(y * 16) + x].set_rgb(r, g, b);
+    pub fn set_pixel(&mut self, x: usize, y: usize, c: rgb::RGB8) {
+        self.leds[(y * 16) + x] = c;
     }
 
     /// Return a tuple of an individual pixel's RGB value.
@@ -132,8 +136,8 @@ impl UnicornHatHd {
     ///
     /// *NOTE*: This returns what's in the display buffer, not what the
     /// physical pixel is set to.
-    pub fn get_pixel(&self, x: usize, y: usize) -> (u8, u8, u8) {
-        self.leds[(y * 16) + x].get_rgb()
+    pub fn get_pixel(&self, x: usize, y: usize) -> rgb::RGB8 {
+        self.leds[(y * 16) + x]
     }
 
     /// Clear the internal buffer of pixel states.
@@ -141,7 +145,7 @@ impl UnicornHatHd {
     /// To clear the display itself, you'll still need to call
     /// [`display`](#method.display) to update the Unicorn HAT HD.
     pub fn clear_pixels(&mut self) {
-        self.leds = [UnicornHatHdLed::default(); 256];
+        self.leds = [BLACK; 256];
     }
 
     #[cfg(feature = "hardware")]
@@ -162,20 +166,19 @@ impl UnicornHatHd {
             // 4 5 6 => 4 5 6 => 1 2 3 4 5 6 7 8 9
             // 7 8 9    7 8 9
             Rotate::RotNone => for led in self.leds.iter() {
-                let (r, g, b) = led.get_rgb();
-                arr.push(r);
-                arr.push(g);
-                arr.push(b);
+                arr.push(led.r);
+                arr.push(led.g);
+                arr.push(led.b);
             },
             // 1 2 3    7 4 1
             // 4 5 6 => 8 5 2 => 7 4 1 8 5 2 9 6 3
             // 7 8 9    9 6 3
             Rotate::RotCW90 => for x in 0..16 {
                 for y in (0..16).rev() {
-                    let (r, g, b) = self.get_pixel(x, y);
-                    arr.push(r);
-                    arr.push(g);
-                    arr.push(b);
+                    let led = self.get_pixel(x, y);
+                    arr.push(led.r);
+                    arr.push(led.g);
+                    arr.push(led.b);
                 }
             },
             // 1 2 3    3 6 9
@@ -183,20 +186,19 @@ impl UnicornHatHd {
             // 7 8 9    1 4 7
             Rotate::RotCCW90 => for x in (0..16).rev() {
                 for y in 0..16 {
-                    let (r, g, b) = self.get_pixel(x, y);
-                    arr.push(r);
-                    arr.push(g);
-                    arr.push(b);
+                    let led = self.get_pixel(x, y);
+                    arr.push(led.r);
+                    arr.push(led.g);
+                    arr.push(led.b);
                 }
             },
             // 1 2 3    9 8 7
             // 4 5 6 => 6 5 4 => 9 8 7 6 5 4 3 2 1
             // 7 8 9    3 2 1
             Rotate::Rot180 => for led in self.leds.iter().rev() {
-                let (r, g, b) = led.get_rgb();
-                arr.push(r);
-                arr.push(g);
-                arr.push(b);
+                arr.push(led.r);
+                arr.push(led.g);
+                arr.push(led.b);
             },
         }
 
@@ -210,30 +212,5 @@ impl Default for UnicornHatHd {
     /// This will panic if the default path is not usable.
     fn default() -> UnicornHatHd {
         UnicornHatHd::new("/dev/spidev0.0").unwrap()
-    }
-}
-
-#[derive(Clone, Copy)]
-struct UnicornHatHdLed {
-    r: u8,
-    b: u8,
-    g: u8,
-}
-
-impl UnicornHatHdLed {
-    pub fn set_rgb(&mut self, r: u8, g: u8, b: u8) {
-        self.r = r;
-        self.g = g;
-        self.b = b;
-    }
-
-    pub fn get_rgb(&self) -> (u8, u8, u8) {
-        (self.r, self.g, self.b)
-    }
-}
-
-impl Default for UnicornHatHdLed {
-    fn default() -> UnicornHatHdLed {
-        UnicornHatHdLed { r: 0, g: 0, b: 0 }
     }
 }
